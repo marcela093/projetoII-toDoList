57f10ab12b8885c9a565d97b4222d49c0788da48
josisantiago <josianesborrego@gmail.com>
2019-10-01 16:03:32 -0300

    adicionei elementos na lista

--- js/script.js
+++ js/script.js
@@ -1,27 +1,31 @@
-// FASE 1
-// Adicionar itens na lista
-// criar uma let para guardar a mensagem digitada pelo usuário
-let formulario = document.getElementById("formulario");
-let lista = document.getElementById("lista")
-// criar evento para capturar tarefa
-lista.addEventListener("submit"), function(evento){
-    // capturar input
+const formulario = document.getElementById("formulario");
+const lista = document.getElementById("lista");
+formulario.addEventListener("submit", function(evento){
+    evento.preventDefault();
     let inputUsuario = document.querySelector(".input-de-texto");
     let tarefa = inputUsuario.value;
-    let li = document.createElement("li");
-    lista.appendChild(li);
-    li.textContent = tarefa;
-    // Limpar campo de texto depois que insere o item na lista
-    formulario.reset();
-    // criar uma condição se não for digitado nenhuma tarefa
+    
+
+    let erro = document.querySelector("#erro");
     if (inputUsuario.value.trim() == "") {
         let msgerro = document.createElement("p");
-        let erro = document.querySelector("#erro");
             erro.appendChild(msgerro);
             erro.textContent = "Digite a tarefa";
-    }
-}
-
+            erro.classList.add("erro");
+                 
+    }else {
+        
+        let listaTarefa = document.createElement("li");
+        lista.appendChild(listaTarefa);
+        listaTarefa.textContent = tarefa;
+        erro.textContent = ""; 
+        let confere = document.createElement("p");
+        listaTarefa.appendChild(confere);
+        confere.textContent = "x";
+        formulario.reset();  
+    }   
+           
+})
 
 
 

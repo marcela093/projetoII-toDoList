f2f4eeb0e5574497a300c88516d09f0b5c6f9d0a
josisantiago <josianesborrego@gmail.com>
2019-10-02 17:05:43 -0300

    adicionei drag and drop

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-
-
 const formulario = document.getElementById("formulario");
 const lista = document.getElementById("lista");
+// let caixaTexto = document.querySelector("#campo");
+const contentTarefa = document.getElementById("content__tarefas");
 
 formulario.addEventListener("submit", function(evento){
     evento.preventDefault();
-    let inputUsuario = document.querySelector(".input-de-texto");
-    let tarefa = inputUsuario.value;
+    const inputUsuario = document.querySelector(".input-de-texto");
+    const tarefa = inputUsuario.value;
 
-    let erro = document.querySelector("#erro");
+    const erro = document.querySelector("#erro");
     if (inputUsuario.value.trim() == "") {
         let msgerro = document.createElement("p");
             erro.appendChild(msgerro);
@@ -19,6 +19,7 @@ formulario.addEventListener("submit", function(evento){
         let listaTarefa = document.createElement("li");
         lista.appendChild(listaTarefa);
         listaTarefa.textContent = tarefa;
+        listaTarefa.setAttribute("class", "linha");
         erro.textContent = ""; 
         let confere = document.createElement("p");
         listaTarefa.appendChild(confere);
@@ -26,18 +27,44 @@ formulario.addEventListener("submit", function(evento){
         listaTarefa.addEventListener("click", function(evento){
         listaTarefa.classList.add("excluir");
         })    
+
         formulario.reset();  
-    }
+
+        lista.setAttribute("draggable", true);
+        listaTarefa.setAttribute("draggable", true);
+        contentTarefa.setAttribute("draggable", true);
+
+        lista.addEventListener("dragstart", function(ev) {
+            console.log("dragstart");
+            dragging = ev.target.closest(".linha");
+        })
+
+        lista.addEventListener("dragover", function(ev) {
+            console.log("dragover");
+            ev.preventDefault();
+            const node = ev.target.closest(".linha");
+            this.insertBefore(dragging, node);
+        })
+
+        lista.addEventListener("dragend", function(ev) {
+            dragging = null;
+        })    
+    }    
 })
 
-const maeDosBotoes = document.getElementById("botoes");
-const botaoFeito = document.createElement("button");
-maeDosBotoes.appendChild(botaoFeito);
-botaoFeito.textContent = "Todas feitas";
-botaoFeito.classList.add("tarefa_feita");
-botaoFeito.addEventListener("click", function(evento){
-lista.classList.add("excluir");
+    const maeDosBotoes = document.getElementById("botoes");
+    const botaoFeito = document.createElement("button");
+    maeDosBotoes.appendChild(botaoFeito);
+    botaoFeito.textContent = "Todas feitas";
+    botaoFeito.classList.add("tarefa_feita");
+    botaoFeito.addEventListener("click", function(evento){
+    lista.classList.add("excluir");
 })
 
 
-    
+
+
+
+    // definir o elemento que eu quero que seja arrastado
+    // os elementos têm que estar em uma div para quer eu possa selecioná-los
+    // dar uma classe para a div
